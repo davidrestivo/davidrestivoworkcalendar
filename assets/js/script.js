3e61b6a77feb10059bf6f1481d4d755eeb799bdc
David Restivo <davidrestivo27@gmail.com>
2022-10-31 15:24:59 -0400

    fixed recall of local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -43,15 +43,15 @@ $(document).ready(function () {
     
     // recalling the entered itemed from local storage after a refresh of the page before  a clear of local storage is made
 
-    $("#block9.description").val(localStorage.getItem("block9"));
-    $("#block10.description").val(localStorage.getItem("block10"));
-    $("#block11.description").val(localStorage.getItem("block11"));
-    $("#block12.description").val(localStorage.getItem("block12"));
-    $("#block13.description").val(localStorage.getItem("block13"));
-    $("#block14.description").val(localStorage.getItem("block14"));
-    $("#block15.description").val(localStorage.getItem("block15"));
-    $("#block16.description").val(localStorage.getItem("block16"));
-    $("#block17.description").val(localStorage.getItem("block17"));
+    $("#block9 .description").val(localStorage.getItem("block9"));
+    $("#block10 .description").val(localStorage.getItem("block10"));
+    $("#block11 .description").val(localStorage.getItem("block11"));
+    $("#block12 .description").val(localStorage.getItem("block12"));
+    $("#block13 .description").val(localStorage.getItem("block13"));
+    $("#block14 .description").val(localStorage.getItem("block14"));
+    $("#block15 .description").val(localStorage.getItem("block15"));
+    $("#block16 .description").val(localStorage.getItem("block16"));
+    $("#block17 .description").val(localStorage.getItem("block17"));
 
     currTime();
 
